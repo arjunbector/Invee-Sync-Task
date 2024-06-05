90d40cfc94f253bc3c02ebc5bb409c6f0b269a15
arjunbector <arjunbector@gmail.com>
2024-06-05 14:54:34 +0530

    Add new dummy orders to data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,5 +1,4 @@
 export const data = {
-
     "orders": [
         {
             "id": 1,
@@ -11,6 +10,16 @@ export const data = {
             "status": "Pending"
         },
         {
+            "id": 4,
+            "customer": "Customer D",
+            "items": [
+                { "id": 1, "name": "Item 1", "quantity": 1 },
+                { "id": 2, "name": "Item 2", "quantity": 2 },
+                { "id": 3, "name": "Item 3", "quantity": 3 }
+            ],
+            "status": "Completed"
+        },
+        {
             "id": 2,
             "customer": "Customer B",
             "items": [
@@ -18,12 +27,22 @@ export const data = {
                 { "id": 3, "name": "Item 3", "quantity": 1 }
             ],
             "status": "Completed"
+        },
+        {
+            "id": 3,
+            "customer": "Customer C",
+            "items": [
+                { "id": 2, "name": "Item 2", "quantity": 4 },
+                { "id": 3, "name": "Item 3", "quantity": 2 }
+            ],
+            "status": "Pending"
         }
     ],
     "items": [
         { "id": 1, "name": "Item 1", "stock": 20 },
         { "id": 2, "name": "Item 2", "stock": 15 },
-        { "id": 3, "name": "Item 3", "stock": 10 }
+        { "id": 3, "name": "Item 3", "stock": 10 },
+        { "id": 4, "name": "Item 4", "stock": 25 },
+        { "id": 5, "name": "Item 5", "stock": 30 }
     ]
-
 }
