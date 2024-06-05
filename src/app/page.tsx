e3bfae5400da306c10282c930a5c7d511bdcdb74
arjunbector <arjunbector@gmail.com>
2024-06-05 17:41:46 +0530

    feat: add options at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,36 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <MaxWidthWrapper className="flex min-h-screen flex-col items-center justify-center">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-center text-green-500 text-xl">Welcome to InveeSync</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h1 className="text-center">Select an option to proceed</h1>
+            <div className="mt-1.5 flex flex-wrap gap-2">
+              <Link
+                href="/orders"
+                className={cn(buttonVariants({ variant: "outline" }))}
+              >
+                See Orders
+              </Link>
+              <Link
+                href="/inventory"
+                className={cn(buttonVariants({ variant: "outline" }))}
+              >
+                See Items
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </MaxWidthWrapper>
+    </main>
   );
 }
